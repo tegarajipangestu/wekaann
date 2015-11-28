000a9858d05d511385d39f9e3830f5638f1446ef
felicia-christie <waxrain1217@gmail.com>
2015-11-28 22:44:09 +0700

    ... begitulah

--- ML-ANN/src/ml/ann/MainPTR.java
+++ ML-ANN/src/ml/ann/MainPTR.java
@@ -18,104 +18,121 @@ import weka.filters.supervised.attribute.NominalToBinary;
  * @author Ivana Clairine
  */
 public class MainPTR {
-    
-    public static NominalToBinary m_nominalToBinaryFilter;
-    
-    public static void main(String[] args) throws FileNotFoundException, IOException, Exception{
-        boolean randomWeight;
-        double weightawal = 0.0;
-        m_nominalToBinaryFilter = new NominalToBinary();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Lokasi file: ");
-	  
-        String filepath = in.nextLine();
-	  filepath = "C:\\Users\\SPM\\git\\wkn2\\wekaann\\ML-ANN\\test-arffs\\iris.2D.arff";
-        System.out.println("--- Algoritma ---");
-        System.out.println("1. Perceptron Training Rule");
-        System.out.println("2. Delta Rule Gradient");
-        System.out.println("3. Delta Batch Rule");
-        System.out.println("Pilihan Algoritma (1/2/3) : ");
-        int choice = in.nextInt();
-        String temp = in.nextLine();
-        
-        System.out.println("Apakah Anda ingin memasukkan nilai weight awal? (YES/NO)");
-        String isRandom = in.nextLine();
-        
-        System.out.println("isRandom: "+isRandom);
-        
-        if(isRandom.equals("YES"))
-            randomWeight = false;
-        else randomWeight = true;
-        
-        if(randomWeight == false)
-        {
-            System.out.println("Masukkan nilai weight awal: ");
-            weightawal = in.nextDouble();
-        }
-        
-        FileReader trainreader = new FileReader(filepath);
-        Instances train = new Instances(trainreader);
-        train.setClassIndex(train.numAttributes()-1);
-        m_nominalToBinaryFilter.setInputFormat(train);
-        train = Filter.useFilter(train,m_nominalToBinaryFilter);
-        double[][] input;
-        input = new double[train.numInstances()][train.numAttributes()];
-        for(int i=0; i<train.numInstances(); i++)
-        {
-            for(int j = 1; j < train.numAttributes(); j++)
-            {
-                System.out.println(train.attribute(j-1));
-                input[i][j] = train.instance(i).value(j-1);
-                System.out.println("input["+i+"]["+j+"]: "+ input[i][j]);
-            }
-        }
-        
-        double[] target = new double[train.numInstances()];
-        
-        for(int i=0; i<train.numInstances(); i++)
-        {
-            target[i] = train.instance(i).classValue();
-            System.out.println("target["+i+"]: "+ target[i]);
-        }
-        
-        double[][] weight = new double[train.numAttributes()][1];
-        for(int i=0; i<train.numAttributes(); i++)
-        {
-            weight[i][0] = weightawal;
-        }
-        
-        if(choice == 1)
-        {
-            SinglePTR testrun;
-            testrun = new SinglePTR(train.numInstances(), train.numAttributes()-1, 10, 0.1, 0.01, input, target, weight, 1, randomWeight);
-        }
-        else if(choice == 2)
-        {
-            SinglePTR testrun;
-            testrun = new SinglePTR(train.numInstances(), train.numAttributes()-1, 10, 0.1, 0.01, input, target, weight, 2, randomWeight);
-        }
-        else if(choice == 3)
-        {
-            SinglePTR testrun;
-            testrun = new SinglePTR(train.numInstances(), train.numAttributes()-1, 10, 0.1, 0.01, input, target, weight, 3, randomWeight);
-        }
-        
-//        for(int it = 0; it<train.numInstances(); it++)
-//        {
-//            for(int i=0; i<=train.numAttributes(); i++)
-//            {
-//                System.out.println(testrun.input[it][i]);
-//            }
-//        }
-        
-//        for(int i=0; i<train.numInstances(); i++)
-//        {
-//            for(int j=0;j<train.numAttributes(); j++)
-//            System.out.println("Instance " + i + ", input "+ j + ", value: " + testrun.input[i][j]);
-//            System.out.println("Target "+i+": "+testrun.target[i]);
-//        }
-        
-        
-    }
-    
+
+	public static NominalToBinary m_nominalToBinaryFilter;
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+		boolean randomWeight;
+		double weightawal = 0.0;
+		double learningRate = 0.1;
+		double threshold = 0.01;
+		int maxEpoch = 10;
+
+		m_nominalToBinaryFilter = new NominalToBinary();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Lokasi file: ");
+
+		String filepath = in.nextLine();
+		filepath = "C:\\Program Files (x86)\\Weka-3-7\\data\\iris.2D.arff";
+		System.out.println("--- Algoritma ---");
+		System.out.println("1. Perceptron Training Rule");
+		System.out.println("2. Delta Rule Gradient");
+		System.out.println("3. Delta Batch Rule");
+		System.out.println("Pilihan Algoritma (1/2/3) : ");
+		int choice = in.nextInt();
+		String temp = in.nextLine();
+
+		System.out.println("Apakah Anda ingin memasukkan nilai weight awal? (YES/NO)");
+		String isRandom = in.nextLine();
+
+		if (isRandom.equals("YES")) {
+			randomWeight = false;
+		} else {
+			randomWeight = true;
+		}
+
+		if (randomWeight == false) {
+			System.out.println("Masukkan nilai weight awal: ");
+			weightawal = in.nextDouble();
+		}
+
+		//print config
+		if (isRandom.equalsIgnoreCase("yes")) {
+			System.out.print("isRandom | ");
+		} else {
+			System.out.print("Weight " + weightawal + " | ");
+		}
+
+		System.out.print("L.rate " + learningRate + " | ");
+		System.out.print("Max Epoch " + maxEpoch + " | ");
+		System.out.print("Threshold " + threshold + " | ");
+		System.out.println();
+
+		FileReader trainreader = new FileReader(filepath);
+		Instances train = new Instances(trainreader);
+		train.setClassIndex(train.numAttributes() - 1);
+		m_nominalToBinaryFilter.setInputFormat(train);
+		train = Filter.useFilter(train, m_nominalToBinaryFilter);
+		double[][] input;
+		input = new double[train.numInstances()][train.numAttributes()];
+		for (int i = 0; i < train.numInstances(); i++) {
+			for (int j = 1; j < train.numAttributes(); j++) {
+				System.out.println(train.attribute(j - 1));
+				input[i][j] = train.instance(i).value(j - 1);
+				System.out.println("input[" + i + "][" + j + "]: " + input[i][j]);
+			}
+		}
+		double[][] weight = new double[train.numAttributes()][1];
+		for (int i = 0; i < train.numAttributes(); i++) {
+			weight[i][0] = weightawal;
+		}
+		System.out.println("N.Class: " + train.get(1).numClasses());
+		if (train.get(1).numClasses() == 1) {
+			//init weight for numeric
+			double[] target = new double[train.numInstances()];
+
+			for (int i = 0; i < train.numInstances(); i++) {
+				target[i] = train.instance(i).classValue();
+//			for (int j = 0; j < train.instance(i).numAttributes(); j++)
+//				System.out.print(train.instance(i).stringValue(j) + " ");
+				System.out.println("target[" + i + "]: " + target[i]);
+			}
+
+			if (choice == 1) {
+				SinglePTR testrun;
+				testrun = new SinglePTR(train.numInstances(), train.numAttributes() - 1, maxEpoch, learningRate, threshold, input, target, weight, 1, randomWeight);
+			} else if (choice == 2) {
+				SinglePTR testrun;
+				testrun = new SinglePTR(train.numInstances(), train.numAttributes() - 1, maxEpoch, learningRate, threshold, input, target, weight, 2, randomWeight);
+			} else if (choice == 3) {
+				SinglePTR testrun;
+				testrun = new SinglePTR(train.numInstances(), train.numAttributes() - 1, maxEpoch, learningRate, threshold, input, target, weight, 3, randomWeight);
+			}
+		} else {
+			//init weight for multiclass
+			System.out.println(train.numClasses());
+			double[][] target = new double[train.numInstances()][train.numClasses()];
+			for (int i = 0; i < train.numInstances(); i++){
+				for (int j = 0; j < train.numClasses(); j++){
+					target[i][j] = 0;
+				}
+				System.out.println(train.get(i).classValue());
+				target[i][(int) train.get(i).classValue()] = 1;
+				
+				
+			}
+			
+				MultiClassPTR testrun;
+				testrun = new MultiClassPTR(train.numInstances(), train.numAttributes() - 1, train.get(1).numClasses(), input, weight, target, maxEpoch, learningRate, threshold, choice, randomWeight);
+				
+				double[] testClassify;
+				testClassify = new double[train.numAttributes() - 1];
+				testClassify[0] = 1.4;
+				testClassify[1] = 0.2;
+				
+				testrun.classifyInstance(testClassify);
+		}
+
+	}
+
 }
